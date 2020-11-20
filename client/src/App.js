9916f6c4180dbe44e58347311c2290bcb156d9c5
TS22082 <ts22082@gmail.com>
2020-11-20 14:35:21 -0800

    check for token

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,47 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import UserContext from "./Context/UserContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 import Landing from "./pages/Landing";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import axios from "axios";
 
 function App() {
+  const [userData, setUserData] = useState({
+    user: undefined,
+    token: undefined,
+  });
+
+  const checkLoggedIn = async () => {
+    let token = localStorage.getItem("auth-token");
+
+    if (token === null) {
+      localStorage.setItem("auth-token", "");
+      token = "";
+    }
+
+    const tokenRes = await axios.post("/users/tokenIsValid", null, {
+      headers: { "x-auth-token": token },
+    });
+
+    if (tokenRes.data) {
+      const userRes = await axios.get("/users/", {
+        headers: { "x-auth-token": token },
+      });
+
+      setUserData({
+        token,
+        user: userRes.data,
+      });
+    }
+  };
+
+  useEffect(() => {
+    checkLoggedIn();
+  }, []);
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -22,10 +58,12 @@ function App() {
         </Navbar>
         {/* Main Content */}
         <Container>
-          <Switch>
-            <Route path="/home" component={Home} />
-            <Route path="/" component={Landing} />
-          </Switch>
+          <UserContext.Provider value={{ userData, setUserData }}>
+            <Switch>
+              <Route path="/home" component={Home} />
+              <Route path="/" component={Landing} />
+            </Switch>
+          </UserContext.Provider>
         </Container>
       </BrowserRouter>
     </div>
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -7,9 +7,6 @@ const Register = () => {
       <Form.Group>
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
       </Form.Group>
 
       <Form.Group>
