9bb45778a23509f7dab4bb77ab94599cd4e4a35b
TS22082 <ts22082@gmail.com>
2020-11-20 11:59:14 -0800

    set up client side routes for landing and home

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar, Nav } from "react-bootstrap";
+import Home from "./pages/Home";
+import Landing from "./pages/Landing";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">Coffee Logs</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <BrowserRouter>
+        {/* Navigation */}
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Navbar.Brand href="#home">Coffee Logs</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        {/* Main Content */}
+        <Container>
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </Container>
+      </BrowserRouter>
     </div>
   );
 }
