a3879cf02ca0982abbd3d223373c08d9e6e99a67
TS22082 <ts22082@gmail.com>
2020-11-20 13:47:05 -0800

    set up login and register card with tabs

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Col, Row, Card, Nav } from "react-bootstrap";
 
 const Landing = () => {
+  const styles = {
+    card: {
+      marginTop: "20px",
+    },
+  };
+
+  const [landing, setLanding] = useState("login");
+
+  const handleSelect = (eventKey) => setLanding(eventKey);
+
   return (
-    <div>
-      <h1>Landing</h1>
-    </div>
+    <Row xs={12}>
+      <Col xs={{ span: 8, offset: 2 }}>
+        <Card style={styles.card}>
+          <Card.Body>
+            <Nav
+              variant="tabs"
+              defaultActiveKey="login"
+              onSelect={handleSelect}
+            >
+              <Nav.Item>
+                <Nav.Link eventKey="login">Login</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="register">Register</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Card.Body>
+          <Card.Body>
+            {landing === "login" ? <h1>login</h1> : <h1>register</h1>}
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
