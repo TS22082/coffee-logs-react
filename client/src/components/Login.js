defd3dbb2f4ac45699f63d2fe67254b1694566cf
TS22082 <ts22082@gmail.com>
2020-11-20 14:48:13 -0800

    set up login and register state

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
 const Login = () => {
+  const [login, setLogin] = useState({});
+
+  const onChange = (e) => {
+    setLogin({ ...login, [e.target.name]: e.target.value });
+  };
+
   return (
     <Form>
       <Form.Group>
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          name="email"
+          onChange={onChange}
+          type="email"
+          placeholder="Enter email"
+        />
       </Form.Group>
 
       <Form.Group>
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          name="password"
+          onChange={onChange}
+          type="password"
+          placeholder="Password"
+        />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,27 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
 const Register = () => {
+  const [register, setRegister] = useState({});
+
+  const onChange = (e) => {
+    setRegister({ ...register, [e.target.name]: e.target.value });
+  };
   return (
     <Form>
       <Form.Group>
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          name="email"
+          onChange={onChange}
+          type="email"
+          placeholder="Enter email"
+        />
       </Form.Group>
 
       <Form.Group>
         <Form.Label>Username</Form.Label>
-        <Form.Control type="text" placeholder="Enter username" />
+        <Form.Control
+          name="userName"
+          onChange={onChange}
+          type="text"
+          placeholder="Enter username"
+        />
       </Form.Group>
 
       <Form.Group>
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          name="password"
+          onChange={onChange}
+          type="password"
+          placeholder="Password"
+        />
       </Form.Group>
 
       <Form.Group>
         <Form.Label>Confirm Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          name="passwordCheck"
+          onChange={onChange}
+          type="password"
+          placeholder="Password"
+        />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
