68a0ec9e40968b871b6b9d35ddee51b1e58103f3
TS22082 <ts22082@gmail.com>
2020-11-20 10:29:23 -0800

    setup basic server and connect to mongodb

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/coffee-logs",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB connection established");
+  }
+);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => console.log(`Listening at: http://localhost:${PORT}`));
