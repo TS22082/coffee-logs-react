92330a99a657e670f224b148dc4af91d6310b166
TS22082 <ts22082@gmail.com>
2020-11-21 21:47:22 -0800

    modify edit screen

--- client/src/pages/Edit.js
+++ client/src/pages/Edit.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Form } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
 
 const Edit = (props) => {
   const [log, setLog] = useState([]);
+  const history = useHistory();
 
   const onChange = (e) => {
     e.preventDefault();
@@ -26,18 +28,36 @@ const Edit = (props) => {
     })();
   }, [props]);
   return (
-    <div>
-      <Form.Group>
-        <Form.Label>Supports Markdown</Form.Label>
-        <Form.Control
-          name="text"
-          onChange={onChange}
-          value={log.text}
-          as="textarea"
-          rows={3}
-        />
-      </Form.Group>
-    </div>
+    <Row className="mt-4">
+      <Col md={{ span: 8, offset: 2 }}>
+        <Form>
+          <Form.Group>
+            <Form.Label>
+              <h2>Edit this log (supports markdown)</h2>
+            </Form.Label>
+            <Form.Control
+              name="text"
+              onChange={onChange}
+              value={log.text}
+              as="textarea"
+              rows={6}
+            />
+          </Form.Group>
+          <Form.Group>
+            <div className="text-right">
+              <Button
+                onClick={() => history.push("/")}
+                variant="secondary"
+                className="mr-2"
+              >
+                Cancel
+              </Button>
+              <Button variant="primary">Save Log</Button>
+            </div>
+          </Form.Group>
+        </Form>
+      </Col>
+    </Row>
   );
 };
 
