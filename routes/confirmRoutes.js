917e79042e70c86bb250a62bc8bffc515244bc08
TS22082 <ts22082@gmail.com>
2020-11-28 12:39:57 -0800

    set key confirmed to true when user follows confirmation link with valid token

--- routes/confirmRoutes.js
+++ routes/confirmRoutes.js
@@ -1,14 +1,22 @@
 const router = require("express").Router();
 const Confirm = require("../models/confirmModel");
+const User = require("../models/userModel");
 
 router.post("/", async (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
 
   try {
-    const confirmationToken = await Confirm.find({ token: req.body.token });
-    console.log(confirmationToken);
-    res.send(confirmationToken);
+    const confirmation = await Confirm.find({ token: req.body.token });
+    console.log(confirmation[0].authorId);
+
+    const confirmedUser = await User.findById(confirmation[0].authorId);
+
+    confirmedUser.confirmed = true;
+    confirmedUser.save();
+
+    res.send("success");
   } catch (err) {
+    console.log(err);
     res.send({ err: "problems" });
   }
 });
