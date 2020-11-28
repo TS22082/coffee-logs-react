209292809ac0b144acc12b87f7d082ba62599846
TS22082 <ts22082@gmail.com>
2020-11-28 12:46:42 -0800

    switch to findOne for Confirm token

--- routes/confirmRoutes.js
+++ routes/confirmRoutes.js
@@ -3,13 +3,11 @@ const Confirm = require("../models/confirmModel");
 const User = require("../models/userModel");
 
 router.post("/", async (req, res) => {
-  // console.log(req.body);
-
   try {
-    const confirmation = await Confirm.find({ token: req.body.token });
-    console.log(confirmation[0].authorId);
+    const confirmation = await Confirm.findOne({ token: req.body.token });
+    console.log(confirmation.authorId);
 
-    const confirmedUser = await User.findById(confirmation[0].authorId);
+    const confirmedUser = await User.findById(confirmation.authorId);
 
     confirmedUser.confirmed = true;
     confirmedUser.save();
