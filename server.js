d6e61ca314dd6c4b002a34be554bcde637e4c463
TS22082 <ts22082@gmail.com>
2021-03-08 15:41:30 -0800

    check production status in server

--- server.js
+++ server.js
@@ -33,8 +33,10 @@ app.use("/users", require("./routes/userRoutes"));
 app.use("/logs", require("./routes/logRoutes"));
 app.use("/confirm", require("./routes/confirmRoutes"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Listening at: http://localhost:${PORT}`));
