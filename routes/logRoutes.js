065c1088be7b41e8b5bb343948da3f2eff92912f
TS22082 <ts22082@gmail.com>
2021-03-11 11:45:19 -0800

    clean up log routes

--- routes/logRoutes.js
+++ routes/logRoutes.js
@@ -6,17 +6,28 @@ const {
   deleteLog,
   editLog,
 } = require("../controllers/logController");
+
 const auth = require("../middleware/auth");
 const Log = require("../models/logModel");
 
+// create new log
+// ROUTE: /logs/
 router.post("/", auth, newLog);
 
+// get all user logs by auth
+// ROUTE: /logs/
 router.get("/", auth, getUserLogs);
 
+// find specific user log
+// ROUTE: /logs/:id
 router.get("/find/:id", auth, findLog);
 
+// delete specific user log
+// ROUTE: /logs/:id
 router.delete("/:logid", auth, deleteLog);
 
+// edit specific user log, id to edit passed through req.body
+// ROUTE: /logs/edit
 router.patch("/edit", auth, editLog);
 
 module.exports = router;
